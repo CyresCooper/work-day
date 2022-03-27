139ec6daa955ae41dc962441f4acae223e1d64d0
Cyres Cooper <95892453+CyresCooper@users.noreply.github.com>
2022-03-26 21:22:23 -0400

    buttons added

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,29 @@
-var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
-var currentHour = moment().format('h:mm:ss a');
+var currentDay = moment().format("dddd, MMMM DD, YYYY  h:mm a");
+$("#currentDay").text(currentDay);
+var hour = parseInt(moment().format('H'));
+var time = $(".time");
+var saveBtn = $(".saveBtn");
+
+
+//save local storage
+saveBtn.click(function () {
+  plan = $(this).prev().val();
+  block = $(this).prev().attr("id");
+  localStorage.setItem(block, plan);
+});
+//clear local storage
+$("#clearDay").on("click", function () {
+  localStorage.clear();
+  initPage()
+});
+
+$(document).ready(function () {
+  for (var i = 9; i < 18; i++) {
+    var savedPlan = document.getElementById(i)
+    $(savedPlan).text(localStorage.getItem(i));
+    console.log('savedPlan:', savedPlan)
+  }
+});
+
+
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# Work Day Scheduler Starter Code
-# work-day
